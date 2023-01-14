2c8c5b901b962f2e64a97797f17f820c5dbb041f
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-14 12:04:52 +0100

    Add files via upload

---
+++ Cwiczenia 4 Zad 4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Cwiczenia_4_Zad_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+        }   
+    }
+}
